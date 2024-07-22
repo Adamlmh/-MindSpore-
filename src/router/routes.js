93dce1469bdc1f2897655dbfc5542bb77bb83b4b
L-L777 <148612270+L-L777@users.noreply.github.com>
2024-07-22 20:13:50 +0800

    feat layout

--- src/router/routes.js
+++ src/router/routes.js
@@ -1,12 +1,19 @@
-import { lazy } from "react";
+import { Children, lazy } from "react";
 
 // import Main from "../pages/main";
 // import Login from "../pages/login";
 // 动态加载组件
-const Main = lazy(() => import("../pages/main"));
+// const Main = lazy(() => import("../pages/main"));
 const Login = lazy(() => import("../pages/login"));
+const Home = lazy(() => import("../pages/Home"));
+const Guidance = lazy(() => import("../pages/layout/layout"));
 const routers = [
-  { path: "/", element: <Main /> },
+  { 
+    path: "/", element: <Guidance /> ,
+    children: [{ path: "/home", element: <Home /> },]
+
+  },
   { path: "/login", element: <Login /> },
+  
 ];
 export default routers;
