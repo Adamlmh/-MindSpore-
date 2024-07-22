7879e75b4bd9547cb27888fe39b568da9e16f63d
Adamlmh <13798755865@163.com>
2024-07-22 10:33:42 +0800

    feat router&stores

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
-
+// import Router  from "./router/index";
 
 function App() {
   return (
     <div className="App">
+      {/* <Router/> */}
       <h1>My First React App</h1> {/* This is a comment */}
     </div>
   );
---
+++ src/router/index.js
@@ -0,0 +1,4 @@
+import { useRoutes } from "react-router-dom";
+import routes from "./routes.js"
+
+export default function Router() { return useRoutes(routes) }
