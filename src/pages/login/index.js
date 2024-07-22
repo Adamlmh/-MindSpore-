ec4d60e122ee3c3ca23790fb8fbfcf3bdeeba1be
wuminglaogou <2132772270@qq.com>
2024-07-22 15:16:07 +0800

    0

--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -12,39 +12,44 @@ const Login = () => {
   return (
     <Form className="login-container" onFinish={handleSubmit}>
       <div className="login_title">
-        <div className="account_login">账号登录</div>
-        <div className="noaccount_login">免密登录</div>
+        <div className="top_name">万千模型</div>
+        <div className="left_name"></div>
+        <div className="right_name"></div>
+        <span className="account_login">账号登录</span>
+        <span className="noaccount_login">免密登录</span>
       </div>
-      <div className="login_title"></div>
       <Form.Item name="username" className="username">
-        <div>
-          <h3>手机号/用户名/邮箱</h3>
-          <Input placeholder="请输入账号" />
+        <div className="input-container">
+          <label htmlFor="username">手机号/用户名/邮箱</label>
+          <Input id="username" name="username" placeholder="请输入账号" />
         </div>
       </Form.Item>
       <Form.Item name="password" className="password">
-        <div>
-          {" "}
-          <h3>密码</h3>
-          <Input.Password placeholder="请输入账号" />
+        <div className="input-container">
+          <label htmlFor="password">密码</label>
+          <Input.Password id="password" name="password" placeholder="请输入密码" />
         </div>
       </Form.Item>
       <Form.Item className="login-button">
-        <div>
-          <div className="forget_rember">
-            <div className="rember_div">
-              <input type="checkbox" className="rember_choose" />
-              <label className="remberMe">记住我</label>
+        <div className="button-container">
+          <div className="forget-remember">
+            <div className="remember-me">
+              <input type="checkbox" id="rememberMe" className="remember-choose" />
+              <label htmlFor="rememberMe">记住我</label>
             </div>
-            <span className="forgetPassword">忘记密码</span>
+            <a href="#" className="forget-password">忘记密码</a>
           </div>
-          <div className="button_click">
-            <Button onClick={handleSubmit} className="sing_up_button">
+          <div className="button-click">
+            <Button type="default" className="sign-up-button">
               注册
-            </Button>
-            <Button onClick={handleSubmit} className="sing_in_button">
+        </Button>
+            <Button type="primary" htmlType="submit" className="sign-in-button">
               登录
-            </Button>
+        </Button>
+          </div>
+          <div className="agree-terms">
+            <input type="checkbox" id="agreeTerms" />
+            <label className="agreeTerms">已同意<a href="#">《服务条款》</a>和<a href="#">《隐私政策》</a></label>
           </div>
         </div>
       </Form.Item>
