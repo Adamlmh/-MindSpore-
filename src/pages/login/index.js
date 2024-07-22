57ca3eeb7ec2378b1a4c18d43e70221ec75440cf
wuminglaogou <2132772270@qq.com>
2024-07-22 22:18:14 +0800

    newssss

--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -1,59 +1,74 @@
 import React from "react";
 import { Form, Input, Button, message } from "antd";
 import "./login.css";
-import { useNavigate, Navigate } from "react-router-dom";
+import { useNavigate, Navigate, NavLink } from "react-router-dom";
 
 const Login = () => {
   const navigate = useNavigate();
   const handleSubmit = () => {
     navigate("/");
   };
+  const jump_to_noaccount_login = () => {
+    navigate("/noaccount", { replace: true });
+  };
+  const jump_to_register = () => {
+    navigate("/register", { replace: true });
+  };
+
+  //手动修改图片
+  const backgroundRef = React.useRef()
+  const backgroundImage = require('../../assest/images/login.jpg')
+  React.useEffect(() => {
+    backgroundRef.current.style.backgroundImage = `url(${backgroundImage})`
+  })
 
   return (
-    <Form className="login-container" onFinish={handleSubmit}>
-      <div className="login_title">
-        <div className="top_name">万千模型</div>
-        <div className="left_name"></div>
-        <div className="right_name"></div>
-        <span className="account_login">账号登录</span>
-        <span className="noaccount_login">免密登录</span>
-      </div>
-      <Form.Item name="username" className="username">
-        <div className="input-container">
-          <label htmlFor="username">手机号/用户名/邮箱</label>
-          <Input id="username" name="username" placeholder="请输入账号" />
+    <div className="Big_Div" ref={backgroundRef}>
+      <Form className="login-container" onFinish={handleSubmit}>
+        <div className="login_title">
+          <div className="top_name">万千模型</div>
+          <div className="left_name"></div>
+          <div className="right_name"></div>
+          <span className="account_login active">账号登录</span>
+          <span className="noaccount_login" onClick={jump_to_noaccount_login} >免密登录</span>
         </div>
-      </Form.Item>
-      <Form.Item name="password" className="password">
-        <div className="input-container">
-          <label htmlFor="password">密码</label>
-          <Input.Password id="password" name="password" placeholder="请输入密码" />
-        </div>
-      </Form.Item>
-      <Form.Item className="login-button">
-        <div className="button-container">
-          <div className="forget-remember">
-            <div className="remember-me">
-              <input type="checkbox" id="rememberMe" className="remember-choose" />
-              <label htmlFor="rememberMe">记住我</label>
+        <Form.Item name="username" className="username">
+          <div className="input-container">
+            <label htmlFor="username">手机号/用户名/邮箱</label>
+            <Input id="username" name="username" placeholder="请输入账号" />
+          </div>
+        </Form.Item>
+        <Form.Item name="password" className="password">
+          <div className="input-container">
+            <label htmlFor="password">密码</label>
+            <Input.Password id="password" name="password" placeholder="请输入密码" />
+          </div>
+        </Form.Item>
+        <Form.Item className="login-button">
+          <div className="button-container">
+            <div className="forget-remember">
+              <div className="remember-me">
+                <input type="checkbox" id="rememberMe" className="remember-choose" />
+                <label htmlFor="rememberMe" className="rememberMe_text">记住我</label>
+
+              </div>
             </div>
-            <a href="#" className="forget-password">忘记密码</a>
-          </div>
-          <div className="button-click">
-            <Button type="default" className="sign-up-button">
-              注册
+            <div className="button-click">
+              <Button type="default" className="sign-up-button" onClick={jump_to_register}  >
+                注册
         </Button>
-            <Button type="primary" htmlType="submit" className="sign-in-button">
-              登录
+              <Button type="primary" htmlType="submit" className="sign-in-button">
+                登录
         </Button>
+            </div>
+            <div className="agree-terms">
+              <input type="checkbox" id="agreeTerms" />
+              <label className="agreeTerms" className="agree_text">已同意<a href="#">《服务条款》</a>和<a href="#">《隐私政策》</a></label>
+            </div>
           </div>
-          <div className="agree-terms">
-            <input type="checkbox" id="agreeTerms" />
-            <label className="agreeTerms">已同意<a href="#">《服务条款》</a>和<a href="#">《隐私政策》</a></label>
-          </div>
-        </div>
-      </Form.Item>
-    </Form>
+        </Form.Item>
+      </Form>
+    </div>
   );
 };
 
--- src/router/routes.js
+++ src/router/routes.js
@@ -5,8 +5,12 @@ import { lazy } from "react";
 // 动态加载组件
 const Main = lazy(() => import("../pages/main"));
 const Login = lazy(() => import("../pages/login"));
+const Register = lazy(() => import("../pages/register"))
+const NoAccount = lazy(() => import("../pages/loginnoaccount"))
 const routers = [
   { path: "/", element: <Main /> },
   { path: "/login", element: <Login /> },
+  { path: "/register", element: <Register /> },
+  { path: "/noaccount", element: <NoAccount /> }
 ];
 export default routers;
