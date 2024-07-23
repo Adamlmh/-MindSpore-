b926e9ac31d1642cd059aaa24a8ca270532274dc
Adamlmh <13798755865@163.com>
2024-07-23 17:51:12 +0800

    暂存

---
+++ src/components/Loding.jsx
@@ -0,0 +1,9 @@
+function Loding() {
+  return (
+    <div className="div">
+      <h1>hello</h1>
+    </div>
+  );
+}
+
+export default Loding;
--- src/router/routes.js
+++ src/router/routes.js
@@ -1,24 +1,25 @@
 import { Children, lazy } from "react";
-
-// import Main from "../pages/main";
-// import Login from "../pages/login";
 // 动态加载组件
 const Main = lazy(() => import("../pages/main"));
 const Login = lazy(() => import("../pages/login"));
-const Register = lazy(() => import("../pages/register"))
-const NoAccount = lazy(() => import("../pages/loginnoaccount"))
-const Guidance = lazy(() => import("../pages/layout/layout"))
-const Home = lazy(() => import("../pages/Home"))
+const Register = lazy(() => import("../pages/register"));
+const NoAccount = lazy(() => import("../pages/loginnoaccount"));
+const Guidance = lazy(() => import("../pages/layout/layout"));
+const Home = lazy(() => import("../pages/Home"));
+
 const routers = [
-  { path: "/", element: <Guidance />,
-    children:[
+  {
+    path: "/",
+    element: <Guidance />,
+    children: [
       {
-        path: "/home", element: <Home />,
-      }
-    ]
-   },
+        path: "/home",
+        element: <Home />,
+      },
+    ],
+  },
   { path: "/login", element: <Login /> },
   { path: "/register", element: <Register /> },
-  { path: "/noaccount", element: <NoAccount /> }
+  { path: "/noaccount", element: <NoAccount /> },
 ];
 export default routers;
