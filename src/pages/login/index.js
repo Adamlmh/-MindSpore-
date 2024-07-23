60a35ab53f19649d18c40cb2172579a53b4ffb3c
Adamlmh <13798755865@163.com>
2024-07-23 10:37:48 +0800

    store

--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -16,21 +16,27 @@ const Login = () => {
   };
 
   //手动修改图片
-  const backgroundRef = React.useRef()
-  const backgroundImage = require('../../assest/images/login.jpg')
+  const backgroundRef = React.useRef();
+  const backgroundImage = require("../../assest/images/login.jpg");
   React.useEffect(() => {
-    backgroundRef.current.style.backgroundImage = `url(${backgroundImage})`
-  })
+    backgroundRef.current.style.backgroundImage = `url(${backgroundImage})`;
+  });
 
   return (
     <div className="Big_Div" ref={backgroundRef}>
-      <Form className="login-container" onFinish={handleSubmit}>
+      <Form
+        className="login-container"
+        onFinish={handleSubmit}
+        style={{ paddingLeft: "15px", paddingRight: "15px" }}
+      >
         <div className="login_title">
           <div className="top_name">万千模型</div>
           <div className="left_name"></div>
           <div className="right_name"></div>
           <span className="account_login active">账号登录</span>
-          <span className="noaccount_login" onClick={jump_to_noaccount_login} >免密登录</span>
+          <span className="noaccount_login" onClick={jump_to_noaccount_login}>
+            免密登录
+          </span>
         </div>
         <Form.Item name="username" className="username">
           <div className="input-container">
@@ -41,29 +47,48 @@ const Login = () => {
         <Form.Item name="password" className="password">
           <div className="input-container">
             <label htmlFor="password">密码</label>
-            <Input.Password id="password" name="password" placeholder="请输入密码" />
+            <Input.Password
+              id="password"
+              name="password"
+              placeholder="请输入密码"
+            />
           </div>
         </Form.Item>
         <Form.Item className="login-button">
           <div className="button-container">
             <div className="forget-remember">
               <div className="remember-me">
-                <input type="checkbox" id="rememberMe" className="remember-choose" />
-                <label htmlFor="rememberMe" className="rememberMe_text">记住我</label>
-
+                <input
+                  type="checkbox"
+                  id="rememberMe"
+                  className="remember-choose"
+                />
+                <label htmlFor="rememberMe" className="rememberMe_text">
+                  记住我
+                </label>
               </div>
             </div>
             <div className="button-click">
-              <Button type="default" className="sign-up-button" onClick={jump_to_register}  >
+              <Button
+                type="default"
+                className="sign-up-button"
+                onClick={jump_to_register}
+              >
                 注册
-        </Button>
-              <Button type="primary" htmlType="submit" className="sign-in-button">
+              </Button>
+              <Button
+                type="primary"
+                htmlType="submit"
+                className="sign-in-button"
+              >
                 登录
-        </Button>
+              </Button>
             </div>
             <div className="agree-terms">
               <input type="checkbox" id="agreeTerms" />
-              <label className="agreeTerms" className="agree_text">已同意<a href="#">《服务条款》</a>和<a href="#">《隐私政策》</a></label>
+              <label className="agreeTerms agree_text">
+                已同意<a href="#">《服务条款》</a>和<a href="#">《隐私政策》</a>
+              </label>
             </div>
           </div>
         </Form.Item>
