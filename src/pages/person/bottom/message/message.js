eae8be4a5584476748a997a1c43ff7793f089026
L-L777 <148612270+L-L777@users.noreply.github.com>
2024-07-25 16:15:52 +0800

    feat person msg's page

--- src/pages/person/bottom/message/message.js
+++ src/pages/person/bottom/message/message.js
@@ -1,17 +1,26 @@
 import "./message.css"
-import { Card } from 'antd';
+import { Card, Pagination } from 'antd';
 import MessageCard from "./messagecard";
+const onChange = (pageNumber) => {
+        console.log('Page: ', pageNumber);
+};
 const MessageList = ({ type }) => (
 
-        <div className="messagelist">
-                <MessageCard type={type} status={'1'} num={'10'} />
-                <MessageCard type={type} status={'1'} num={'90'} />
-                <MessageCard type={type} status={'1'} num={'70'} />
-                <MessageCard type={type} status={'0'} num={'30'} />
-                <MessageCard type={type} status={'0'} num={'110'} />
-                <MessageCard type={type} status={'0'} num={'110'} />
-                <MessageCard type={type} status={'0'} num={'110'} />
-                <MessageCard type={type} status={'0'} num={'110'} />
+        <div className="messagebox">
+                <div className="messagelist">
+                        <MessageCard type={type} status={'1'} num={'10'} />
+                        <MessageCard type={type} status={'1'} num={'90'} />
+                        <MessageCard type={type} status={'1'} num={'70'} />
+                        <MessageCard type={type} status={'0'} num={'30'} />
+                        <MessageCard type={type} status={'0'} num={'110'} />
+                        <MessageCard type={type} status={'0'} num={'110'} />
+                        <MessageCard type={type} status={'0'} num={'110'} />
+                        <MessageCard type={type} status={'0'} num={'110'} />
+                </div>
+                <div className="msg_page">
+                        <Pagination showQuickJumper defaultCurrent={2} total={500} align="center" onChange={onChange} />
+                </div>
+                
         </div>
 
 
